4564d83d3364a1c81e3fdf3f11fdc2c908638f63
Mohsin Shoaib <mohsinshoaib735@gmail.com>
2022-08-30 18:35:09 +0500

    Javascript Day 2

---
+++ JS-2/app.js
@@ -0,0 +1,134 @@
+// Type Conversion & type coersion
+
+// let myName = "123";
+// myName = Number(myName);
+// myName = String(myName);
+// myName = true;
+// myName = String(myName);
+
+// console.log(myName.length);
+// console.log(myName);
+// console.log(typeof myName);
+
+// == , ===
+
+// == Loose Equality Operator
+// === Strict Equality Operator
+
+// let firstNumber = 1234;
+
+// console.log(firstNumber.toFixed(3));
+// let secondNumber = "1234";
+
+// console.log(firstNumber === secondNumber);
+
+// let pi = Math.PI;
+// console.log(pi);
+
+// let number = 25;
+// console.log(Math.sqrt(number));
+
+// let number = 2.5;
+// console.log(Math.round(number));
+
+// let number = -5;
+// console.log(Math.abs(number));
+
+// let number = Math.round(Math.random() * 20);
+// console.log(number);
+
+// let firstName = "Mohsin";
+// let lastName = "Shoaib";
+// let age = 22;
+// let gender = "male";
+
+// console.log(
+//   "Hi, there is the message from javascript to " +
+//     firstName +
+//     " " +
+//     lastName +
+//     " whose age is " +
+//     age +
+//     " whose gender is " +
+//     gender
+// );
+
+// // Es 6
+// console.log(
+//   `Hi, there is the message from javascript to ${firstName} ${lastName} whose age is ${age} whose gender is ${gender}`
+// );
+
+// let age = 23;
+
+// if (age === 22) {
+//   console.log("Hello");
+// } else {
+//   console.log("World");
+// }
+
+// let age = 26;
+
+// if (age === 23) {
+//   console.log("23");
+// } else if (age === 24) {
+//   console.log("24");
+// } else if (age === 25) {
+//   console.log("25");
+// } ese {
+//   colnsole.log("Noting");
+// }
+
+// switch (age) {
+//   case 24:
+//     console.log("24");
+//     break;
+//   case 25:
+//     console.log("25");
+//     break;
+//   case 26:
+//     console.log("26");
+//     break;
+//   default:
+//     console.log("Noting");
+// }
+
+// Unary Operators
+// ++ Increment
+// -- Decrement
+
+// Post Increment Post Decrement
+// Pre Increment Pre Decrement
+// let number = 15;
+// console.log(number++);
+// console.log(number);
+
+// console.log(++number);
+
+// LOOPS
+// 1 Initialization
+// 2 Condition
+// 3 Body Execute
+// 4 Increment/Decrement
+
+// For , while , do-while
+
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i);
+// }
+
+// i = 1 ; 1 <= 20 -- T
+// 1
+// i = 2; 2 <= 20 -- T
+// 2
+// i = 3; 3 <= 20 -- T
+// 3 ......... i = 20 ; 20 <= 20 -- T // 20 i = 21; 21 <= 20 -- F
+
+// Print 1 to n even numbers
+
+// let n = 50;
+
+// for (let i = 0; i <= n; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+//   }
+// }
